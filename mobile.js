cb7dc4c1748cab2b555d07cd7498454ee2fe229f
dlams <64592721+dlams@users.noreply.github.com>
2020-11-08 12:31:22 +0900

    Delete mobile.js

--- mobile.js
+++
@@ -1,9 +0,0 @@
-function isMobile() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
-
-if (isMobile()) {
-    alert('모바일');
-} else {
-    alert('PC');
-}
