bc53735909b4c2bf693d15b48e46286b8d117bb1
dlams <64592721+dlams@users.noreply.github.com>
2020-11-08 12:13:43 +0900

    Add files via upload

---
+++ mobile.js
@@ -0,0 +1,9 @@
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+if (isMobile()) {
+    alert('모바일');
+} else {
+    alert('PC');
+}
