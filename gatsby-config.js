77456c667a38101f3687ab64c436cadfff5a3147
이민우 <dlalsdn1009@hyperlunatic.com>
2024-02-01 20:43:37 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ssammwu blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
